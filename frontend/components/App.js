02b7a5c32d153a0c8167b5e5e8546e9ab7a514a8
Pankorn S <sirimanivongp@gmail.com>
2024-11-27 20:59:11 -0700

    safety

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -7,7 +7,8 @@ export default class App extends React.Component {
   state={
     todos:[],
     error:'',
-    taskName:''
+    taskName:'',
+    display:true
   }
   changeHand=evt=>{
     const {value}=evt.target
@@ -36,6 +37,21 @@ export default class App extends React.Component {
       ...this.state,error:err.response.data.message
     })})
   }
+  toggCheck=id=>()=>{
+    axios.patch(`${URL}/${id}`)
+    .then(res=>{
+      this.setState({...this.state,todos:this.state.todos.map(td=>{
+        if(td.id!==id)return td
+        return res.data.data
+      })})
+    })
+    .catch(err=>{this.setState({
+      ...this.state,error:err.response.data.message
+    })})
+  }
+  toggleDis=()=>{
+    this.setState({...this.state,display:!this.state.display})
+  }
   componentDidMount(){
     this.getTodos()
   }
@@ -45,15 +61,18 @@ export default class App extends React.Component {
         <div id='error'>Error: {this.state.error}</div>
         <div id='todos'>
           <h2>Todos:</h2>
-          {this.state.todos.map(td=>{
-            return <div key={td.id}>{td.name}</div>
-          })}
+          {this.state.todos.reduce((acc,td)=>{
+            if(this.state.display||!td.completed)return acc.concat(
+              <div key={td.id} onClick={this.toggCheck(td.id)}>{td.name}{td.completed?'  :D':''}</div>
+            )
+              return acc
+          },[])}
         </div>
         <form id='todoform' onSubmit={this.subHand}>
           <input type='text' placeholder='type todo' value={this.state.taskName} onChange={this.changeHand}></input>
           <input type='submit'></input>
-          <button>Clear</button>
         </form>
+          <button onClick={this.toggleDis}>{this.state.display?'Clear finished tasks':'Show All Tasks'}</button>
       </div>
     )
   }
